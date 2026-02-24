e4a97a75a753e36aeaa5b345a7507c44362b1b94
Perry-Cham <pchamileke@gmail.com>
2026-02-25 00:57:23 +0200

    removed auth keys from file

--- app/d.ts
+++ app/d.ts
@@ -32,7 +32,7 @@ type ProductCategory = "beef" | "pork" | "processed" | "chicken";
 
 async function Migrate(category: ProductCategory){
     const imagekit = new ImageKit({
-        privateKey:"private_S4jl3Yc7h+GsvoUzkz/gDD6Hoj4="
+        privateKey:""
     })
     console.log("created")
     await mongoose.connect("mongodb+srv://perry:Perry2009@cluster0.gja8m9y.mongodb.net/Quicksave?appName=Cluster0")
@@ -49,7 +49,7 @@ async function Migrate(category: ProductCategory){
 
 // New function to update/upsert all pork products
 async function update(category: ProductCategory) {
-  await mongoose.connect("mongodb+srv://perry:Perry2009@cluster0.gja8m9y.mongodb.net/Quicksave?appName=Cluster0");
+  await mongoose.connect("");
   console.log("Connected to MongoDB");
 
     const updatedProducts = JSON.parse(fs.readFileSync('Pdata.json').toString())
